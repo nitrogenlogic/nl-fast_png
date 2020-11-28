efb78371293a3a2787884cee0d3abf5e6c7d8d15
Mike Bourgeous <mike@mikebourgeous.com>
2020-11-28 11:30:23 -0700

    Allow benchmarking different # threads to show parallelism is working.

--- bin/benchmark.rb
+++ bin/benchmark.rb
@@ -7,12 +7,21 @@ require 'benchmark'
 
 require 'nl/fast_png'
 
-data = 256.times.map(&:chr).join * 1024
+THREADS=ENV['THREADS']&.to_i || 8
+COUNT=ENV['COUNT']&.to_i || 1000
+
 elapsed = Benchmark.realtime do
-  10000.times do
-    NL::FastPng.store_png(512, 512, 8, data)
-  end
+  threads = THREADS.times.map {
+    Thread.new do
+      data = 256.times.map(&:chr).join * 1024
+      COUNT.times do
+        NL::FastPng.store_png(512, 512, 8, data)
+      end
+    end
+  }
+
+  threads.each(&:join)
 end
 
 puts "Elapsed: #{elapsed} seconds"
-puts "Speed: #{10000 / elapsed} per second"
+puts "Speed: #{(THREADS * COUNT) / elapsed} per second"
